8d41d4cb57d0c7dd4d54919bcbc131486cf16d19
Lucas de Figueiredo Aguiar <37455976+sendrak@users.noreply.github.com>
2021-07-14 19:41:52 -0300

    Update
    Teste de Conversão

--- exercicios/src/fundamentos/Conversao.java
+++ exercicios/src/fundamentos/Conversao.java
@@ -4,25 +4,29 @@ import java.util.Scanner;
 
 public class Conversao {
 	
-	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.print("Informe o primeiro valor:");
-		double numero1 = Double.parseDouble(scan.nextLine()) ;
+		System.out.print("Informe o Primeiro valor:");
+		String num1 = scan.next().replace(",", ".") ;
 		
-		System.out.print("\nInforme o primeiro valor:");
-		double numero2 = Double.parseDouble(scan.nextLine()) ;
+		System.out.print("\nInforme o Segundo valor:");
+		String num2 = scan.next().replace(",", ".") ;
 		
-		System.out.print("\nInforme o primeiro valor:");
-		double numero3 = Double.parseDouble(scan.nextLine()) ;
+		System.out.print("\nInforme o Terceiro valor:");
+		String num3 = scan.next().replace(",", ".") ;
+		
+		double numero1 = Double.parseDouble(num1);
+		double numero2 = Double.parseDouble(num2);
+		double numero3 = Double.parseDouble(num3);
+		
 		
 		double media = (numero1 + numero2 + numero3) / 3;
 		
 		System.out.println("\nMedia: "+media);
 		
-		
+		scan.close();
 		
 		
 	}
